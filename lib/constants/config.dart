02b240a8a85a25f2a176c06dcf5ac23d22bb3875
Bala <110883394+Bala0035@users.noreply.github.com>
2023-02-08 07:02:18 +0530

    Update config.dart

--- lib/constants/config.dart
+++ lib/constants/config.dart
@@ -1,13 +1,15 @@
 class AppConfig {
   // [Moroz Woo Demo]
-  static const String wooBaseUrl = 'https://dev.moroz.cc/woocommerce/wp-json/wc/v3/';
-  static const String coCartBaseUrl = 'https://dev.moroz.cc/woocommerce/wp-json/cocart/v2/';
+  static const String wooBaseUrl = 'http://34.232.210.136';
+  static const String coCartBaseUrl =
+      'https://dev.moroz.cc/woocommerce/wp-json/cocart/v2/';
   static const String wpBaseUrl = 'https://dev.moroz.cc/woocommerce/wp-json/';
-  static const String authPublicToken = 'ck_2922ed92631924dcaf4af9fe2310c8d266502d8c';
-  static const String authSecretToken = 'cs_dc037ce621691c6d647c32063cba3b9e5cae321f';
+  static const String authPublicToken =
+      'ck_7454790606272e0d575c35c34581650aa5b8e801';
+  static const String authSecretToken =
+      'cs_8060a46d71f2416c31b4f256909b12040e0eeaa0';
   static const String demoCoCartPublicToken = 'ShiftHackZ';
   static const String demoCoCartSecretToken = 'Dd54567109810101';
-
 
   // [DVS Production]
   // static const String wooBaseUrl = 'https://dneprvapeshop.com.ua/wp-json/wc/v3/';
@@ -18,6 +20,6 @@ class AppConfig {
   // static const String demoCoCartPublicToken = 'ShiftHackZ';
   // static const String demoCoCartSecretToken = '46D63TO0rhXbdTR4TqdT7%@7';
 
-  static const String currency = ' грн.';//'\$';
+  static const String currency = ' грн.'; //'\$';
   static const int paginationLimit = 10;
 }
